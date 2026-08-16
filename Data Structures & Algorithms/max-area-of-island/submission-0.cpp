d16a81810fc720ef9237bbd1b7acc50636504a43
Rohit Raj <146508696+RohitRaj37@users.noreply.github.com>
2026-08-16 16:30:05 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    bool check(int x , int y , int n , int m){
+        return (x>=0 && y>=0 && x<n && y<m);
+    }
+    void bfs(vector<vector<int>>& vis, vector<vector<int>>& grid, int i, int j,int &ans) {
+    queue<pair<int, int>> q;
+    int xx = 1 ; 
+    int n = grid.size();
+    int m = grid[0].size();
+    q.push({i, j});
+    vis[i][j] = 1;
+    int dir[5] = {0, 1, 0, -1, 0};
+    while (!q.empty()) {
+        auto curr = q.front();
+        q.pop();
+        for (int k = 0; k < 4; k++) {
+            int x = curr.first + dir[k];
+            int y = curr.second + dir[k + 1];
+            if (check(x, y, n, m) &&
+                vis[x][y] == 0 && grid[x][y]==1) {
+                vis[x][y] = 1;
+                q.push({x, y});
+                xx++;
+            }
+        }
+    }
+    ans = max(ans,xx);
+}
+
+   public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0 ;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] == 0 && grid[i][j]==1) {
+                    bfs(vis, grid, i, j,ans);
+                }
+            }
+        }
+        return ans;
+    }
+};
